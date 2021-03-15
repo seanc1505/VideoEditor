3b04c30dc4ab15d7aa45b0ffd9c88545ba2030c3
seanc1505 <54627825+seanc1505@users.noreply.github.com>
2021-03-15 14:56:49 +0000

    Beginning work on building the image classification model

---
+++ image_classification_0.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout 
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+
+from sklearn.metrics import classification_report,confusion_matrix
+
+import tensorflow as tf
+
+import cv2
+import os
+
+import numpy as np
+
+
+print("Miss u wanna kiss u")
